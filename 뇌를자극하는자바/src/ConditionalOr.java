a8e4ec52dee8b6c6a54fbf512bb3c26b550e54ab
Administrator <Administrator@KHW>
2018-05-01 12:21:17 +0900

    20180501

---
+++ 뇌를자극하는자바/src/ConditionalOr.java
@@ -0,0 +1,17 @@
+
+public class ConditionalOr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 0, num2 = 0;
+		
+		if (++num1 > 0 || ++num2 > 0) {
+			System.out.println("num1이 0보다 크거나 num2가 0보다 큽니다.");
+		}
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+	}
+
+}
