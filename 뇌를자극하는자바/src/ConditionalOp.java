5ba267fca1a84463663d83bc334b98bda6c1f7c1
Administrator <Administrator@KHW>
2018-05-01 12:23:25 +0900

    20180501

---
+++ 뇌를자극하는자바/src/ConditionalOp.java
@@ -0,0 +1,14 @@
+
+public class ConditionalOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 20, b = 30, max;
+		max = a < b ? a : b;
+		
+		System.out.println(max);
+
+	}
+
+}
